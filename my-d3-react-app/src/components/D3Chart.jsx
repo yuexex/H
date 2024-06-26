7a533d229f5fdb7565e81a308efe12a9e91ce6d3
sachemmark <sachemmark@gmail.com>
2024-06-26 21:26:16 +0300

    Revert "inputs experiments"
    This reverts commit 823abdcf2b4b65b319f6bd70b0533d59c94e6c0f.

--- my-d3-react-app/src/components/D3Chart.jsx
+++ my-d3-react-app/src/components/D3Chart.jsx
@@ -1,20 +1,19 @@
 import React, { useEffect, useRef, useState } from "react";
 import * as d3 from "d3";
-import { addNodeOnDblClick } from "../Util/addNodeOnDblClick";
+import { addNodeOnDblClick } from "../Util/addNodeOnDblClick"; // Correct import path
 import ExportButton from "./ExportButton";
 import D3Header from "./D3Header";
 import RemoveButton from "./RemoveButton";
 import styled from "styled-components";
 
+
 const DivColumn = styled.div`
-  display: flex;
   flex-direction: column;
-  justify-content: start;
-  align-items: start;
+  justify-content: center;
+  align-items: center;
 `;
 
 const DivRow = styled.div`
-  display: flex;
   flex-direction: row;
   justify-content: center;
   align-items: center;
@@ -23,9 +22,9 @@ const DivRow = styled.div`
 const D3Chart = ({ width, height, strength }) => {
   const d3Container = useRef(null);
   const [nodes, setNodes] = useState([
-    { id: "A", reflexive: false, label: "A", x: width / 3, y: height / 2 },
-    { id: "B", reflexive: false, label: "B", x: width / 2, y: height / 2 },
-    { id: "C", reflexive: false, label: "C", x: (2 * width) / 3, y: height / 2 },
+    { id: "A", reflexive: false },
+    { id: "B", reflexive: false },
+    { id: "C", reflexive: false },
   ]);
   const [links, setLinks] = useState([
     { source: "A", target: "B", left: false, right: true },
@@ -38,210 +37,163 @@ const D3Chart = ({ width, height, strength }) => {
   useEffect(() => {
     const svg = d3.select(d3Container.current);
 
+    svg.on("dblclick", null);
+    svg.on("click", null);
+
     const dragLine = svg
       .append("path")
       .attr("class", "link dragline hidden")
       .attr("d", "M0,0L0,0")
       .style("marker-end", "url(#end-arrow)");
 
-    svg.on("mousemove", handleMouseMove(dragLine));
-    svg.on("mouseup", handleMouseUp(dragLine));
+    const updateGraph = () => {
+      svg.selectAll("*").remove();
 
-    addNodeOnDblClick(
-      svg,
-      nodes,
-      setNodes,
-      links,
-      setLinks,
-      lastNodeId,
-      setLastNodeId,
-      width,
-      height
-    );
+      const simulation = d3
+        .forceSimulation(nodes)
+        .force(
+          "link",
+          d3
+            .forceLink(links)
+            .id((d) => d.id)
+            .distance(150)
+        )
+        .force("charge", d3.forceManyBody().strength(-strength))
+        .force("center", d3.forceCenter(width / 2, height / 2))
+        .on("tick", ticked);
 
-    const cleanup = updateGraph(svg, nodes, links, dragLine, width, height, strength);
+      const link = svg
+        .append("g")
+        .attr("class", "links")
+        .selectAll("line")
+        .data(links)
+        .enter()
+        .append("line")
+        .attr("stroke-width", 2)
+        .attr("stroke", "#999")
+        .attr("marker-end", "url(#end-arrow)");
+
+      const node = svg
+        .append("g")
+        .attr("class", "nodes")
+        .selectAll("circle")
+        .data(nodes)
+        .enter()
+        .append("circle")
+        .attr("r", 12)
+        .attr("fill", "#69b3a2")
+        .on("mousedown", (event, d) => {
+          if (event.ctrlKey) return;
+          setMousedownNode(d);
+          dragLine
+            .classed("hidden", false)
+            .attr("d", `M${d.x},${d.y}L${d.x},${d.y}`);
+        })
+        .on("mouseup", (event, d) => {
+          if (!mousedownNode) return;
+
+          dragLine.classed("hidden", true);
+
+          const newLink = {
+            source: mousedownNode,
+            target: d,
+            left: false,
+            right: true,
+          };
+          setLinks([...links, newLink]);
+          setMousedownNode(null);
+          updateGraph();
+        })
+        .on("click", (event, d) => {
+          if (selectedNode === null) {
+            setSelectedNode(d);
+          } else {
+            const newLink = {
+              source: selectedNode,
+              target: d,
+              left: false,
+              right: true,
+            };
+            setLinks([...links, newLink]);
+            setSelectedNode(null);
+            updateGraph();
+          }
+        })
+        .call(
+          d3
+            .drag()
+            .on("start", dragstarted)
+            .on("drag", dragged)
+            .on("end", dragended)
+        );
+
+      svg.on("mousemove", function (event) {
+        if (!mousedownNode) return;
+
+        const [x, y] = d3.pointer(event);
+        dragLine.attr("d", `M${mousedownNode.x},${mousedownNode.y}L${x},${y}`);
+      });
+
+      svg.on("mouseup", function () {
+        if (mousedownNode) {
+          dragLine.classed("hidden", true);
+          setMousedownNode(null);
+        }
+      });
+
+      addNodeOnDblClick(
+        svg,
+        nodes,
+        setNodes,
+        links,
+        setLinks,
+        lastNodeId,
+        setLastNodeId
+      );
+
+      function ticked() {
+        link
+          .attr("x1", (d) => d.source.x)
+          .attr("y1", (d) => d.source.y)
+          .attr("x2", (d) => d.target.x)
+          .attr("y2", (d) => d.target.y);
+
+        node.attr("cx", (d) => d.x).attr("cy", (d) => d.y);
+      }
+
+      function dragstarted(event, d) {
+        if (!event.active) simulation.alphaTarget(0.3).restart();
+        d.fx = d.x;
+        d.fy = d.y;
+      }
+
+      function dragged(event, d) {
+        d.fx = event.x;
+        d.fy = event.y;
+      }
+
+      function dragended(event, d) {
+        if (!event.active) simulation.alphaTarget(0);
+        d.fx = null;
+        d.fy = null;
+      }
+
+      return () => {
+        svg.selectAll("*").remove();
+        simulation.stop();
+      };
+    };
+
+    const cleanup = updateGraph();
 
     return () => {
       if (cleanup) cleanup();
     };
-  }, [width, height, strength, lastNodeId]);
-
-  useEffect(() => {
-    const svg = d3.select(d3Container.current);
-    svg.selectAll("*").remove();
-    updateGraph(svg, nodes, links, null, width, height, strength);
-  }, [nodes, links]);
-
-  const updateGraph = (svg, nodes, links, dragLine, width, height, strength) => {
-    const link = createLinks(svg, links);
-    const node = createNodes(svg, nodes, dragLine);
-
-    const simulation = createSimulation(nodes, links, width, height, strength, () => {
-      link
-        .attr("x1", (d) => d.source.x)
-        .attr("y1", (d) => d.source.y)
-        .attr("x2", (d) => d.target.x)
-        .attr("y2", (d) => d.target.y);
-
-      node.attr("transform", (d) => `translate(${d.x},${d.y})`);
-    });
-
-    simulation.alpha(1).restart();
-
-    return () => {
-      svg.selectAll("*").remove();
-      simulation.stop();
-    };
-  };
-
-  const createSimulation = (nodes, links, width, height, strength, ticked) => {
-    return d3
-      .forceSimulation(nodes)
-      .force(
-        "link",
-        d3
-          .forceLink(links)
-          .id((d) => d.id)
-          .distance(150)
-      )
-      .force("charge", d3.forceManyBody().strength(-strength))
-      .force("center", d3.forceCenter(width / 2, height / 2))
-      .on("tick", ticked);
-  };
-
-  const createLinks = (svg, links) => {
-    return svg
-      .append("g")
-      .attr("class", "links")
-      .selectAll("line")
-      .data(links)
-      .enter()
-      .append("line")
-      .attr("stroke-width", 2)
-      .attr("stroke", "#999")
-      .attr("marker-end", "url(#end-arrow)");
-  };
-
-  const createNodes = (svg, nodes, dragLine) => {
-    const node = svg
-      .append("g")
-      .attr("class", "nodes")
-      .selectAll("g")
-      .data(nodes)
-      .enter()
-      .append("g")
-      .attr("transform", (d) => `translate(${d.x},${d.y})`)
-      .call(
-        d3
-          .drag()
-          .on("start", dragstarted)
-          .on("drag", dragged)
-          .on("end", dragended)
-      );
-
-    const foreignObject = node
-      .append("foreignObject")
-      .attr("width", 100)
-      .attr("height", 30)
-      .append("xhtml:div")
-      .style("font", "14px 'Helvetica Neue'")
-      .style("background", "transparent")
-      .style("border", "none")
-      .html((d) => `<input type='text' value='${d.label}' style='width: 100%; background: transparent; border: none;' />`);
-
-    foreignObject
-      .on("mousedown", (event, d) => handleMouseDown(event, d, dragLine))
-      .on("mouseup", (event, d) => handleMouseUpNode(event, d, dragLine))
-      .on("click", (event, d) => handleClick(event, d))
-      .select("input")
-      .each(function(d) {
-        d3.select(this).on("blur", (event) => handleInputBlur(event, d));
-      });
-
-    return node;
-  };
-
-  const handleInputBlur = (event, d) => {
-    const newLabel = event.target.value;
-    setNodes((nodes) =>
-      nodes.map((node) => (node.id === d.id ? { ...node, label: newLabel } : node))
-    );
-    d.label = newLabel; // Update the D3 node data
-    d3.select(event.target).node().value = newLabel; // Update the displayed value
-  };
-
-  const handleMouseMove = (dragLine) => (event) => {
-    if (!mousedownNode) return;
-
-    const [x, y] = d3.pointer(event);
-    dragLine.attr("d", `M${mousedownNode.x},${mousedownNode.y}L${x},${y}`);
-  };
-
-  const handleMouseUp = (dragLine) => () => {
-    if (mousedownNode) {
-      dragLine.classed("hidden", true);
-      setMousedownNode(null);
-    }
-  };
-
-  const handleMouseDown = (event, d, dragLine) => {
-    if (event.ctrlKey) return;
-    if (!dragLine) return;
-    setMousedownNode(d);
-    dragLine
-      .classed("hidden", false)
-      .attr("d", `M${d.x},${d.y}L${d.x},${d.y}`);
-  };
-
-  const handleMouseUpNode = (event, d, dragLine) => {
-    if (!mousedownNode) return;
-    if (!dragLine) return;
-
-    dragLine.classed("hidden", true);
-
-    const newLink = {
-      source: mousedownNode,
-      target: d,
-      left: false,
-      right: true,
-    };
-    setLinks((links) => [...links, newLink]);
-    setMousedownNode(null);
-  };
-
-  const handleClick = (event, d) => {
-    if (selectedNode === null) {
-      setSelectedNode(d);
-    } else {
-      const newLink = {
-        source: selectedNode,
-        target: d,
-        left: false,
-        right: true,
-      };
-      setLinks((links) => [...links, newLink]);
-      setSelectedNode(null);
-    }
-  };
-
-  const dragstarted = (event, d) => {
-    if (!event.active) event.subject.fx = event.subject.x;
-    if (!event.active) event.subject.fy = event.subject.y;
-  };
-
-  const dragged = (event, d) => {
-    event.subject.fx = event.x;
-    event.subject.fy = event.y;
-  };
-
-  const dragended = (event, d) => {
-    if (!event.active) event.subject.fx = null;
-    if (!event.active) event.subject.fy = null;
-  };
+  }, [width, height, nodes, links, mousedownNode, strength, lastNodeId]);
 
   const removeLastNode = () => {
     if (nodes.length > 1) {
-      const newNodes = nodes.slice(0, -1);
+      const newNodes = nodes.slice(0, nodes.length - 1);
       const newLinks = links.filter(
         (link) =>
           link.source.id !== nodes[nodes.length - 1].id &&
@@ -256,7 +208,8 @@ const D3Chart = ({ width, height, strength }) => {
   return (
     <DivColumn>
       <D3Header svgRef={d3Container} width={width} height={height} />
-      <svg ref={d3Container} width={width} height={height}></svg>
+      <svg ref={d3Container}></svg>
+      {/* <h1>Hello</h1> */}
       <DivRow>
         <ExportButton svgRef={d3Container} />
         <RemoveButton onClick={removeLastNode} />
